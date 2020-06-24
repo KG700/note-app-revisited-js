b088ebd7b2f502564b3d39b05d16e91614b59c63
Karla Gardiner <karla.gardiner@gmail.com>
2020-06-24 17:42:33 +0100

    Created a create method in NoteList that creates a new note and adds it to the notes list array, with passing test

--- spec/note-list-model-spec.js
+++ spec/note-list-model-spec.js
@@ -4,13 +4,12 @@ describe("NoteList", () => {
     noteList = new NoteList;
   })
 
-
-  it("has will return the note models stored in the array", () => {
+  it("has will return an empty array array of notes when initialised", () => {
     expect(noteList.all()).toEqual([])
   })
 
-  // it("creates a note and adds it to the list", () => {
-  //   noteList.create("Note 1");
-  //   expect(NoteList.all[0].text).toEqual(["Note 1"])
-  // })
+  it("creates a note and adds it to the list", () => {
+    noteList.create("Note 1");
+    expect(noteList.all()[0].text).toEqual("Note 1")
+  })
 })
--- src/note-list-model.js
+++ src/note-list-model.js
@@ -1,9 +1,17 @@
 ((exports) => {
 
   class NoteList {
+    constructor() {
+      this.notes = []
+    }
 
     all = () => {
-      return [];
+      return this.notes;
+    }
+
+    create = (text) => {
+      note = new Note(text);
+      this.notes.push(note);
     }
   }
 
